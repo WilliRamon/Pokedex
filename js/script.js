4bcf8750275b6aa2284047a77cfbe871d1055359
WilliRamon <acdc.ramon@hotmail.com>
2023-01-29 01:52:39 -0300

    implementando js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-const fetchPokemon = (pokemon) => {
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
 
-    const APIResponse = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    console.log(APIResponse)
+const fetchPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data
 
 }
+
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+
+
+}
+
+renderPokemon('charizard')
